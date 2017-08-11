f4f62c28325634253aa6c7241b9172e1b78dcec7
Xavier Hocquet <xhocquet@gmail.com>
2017-08-11 17:00:27 +0200

    Improved feel/look to receipt upload on front page

---
+++ app/assets/javascripts/application/index.js
@@ -0,0 +1,8 @@
+$(document).on('turbolinks:load', function() {
+  $('.receipt-upload-input').change(function(e) {
+    $(e.currentTarget).parents('form').submit();
+
+    $(e.currentTarget).parents('div.file').addClass('is-hidden');
+    $(e.currentTarget).parents('div.file').siblings('.sk-cube-grid').removeClass('is-hidden');
+  })
+});
