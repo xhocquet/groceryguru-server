9e08eac885bf1f834620097005d31bd0ffebca28
Xavier Hocquet <xhocquet@gmail.com>
2017-08-25 14:56:58 +0200

    Change sorting to grouping so I can group transactions in the view

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,19 +5,18 @@ class StatsController < ApplicationController
                           .completed
                           .group_by(&:item_id)
                           .reject { |item_id, transactions|
-                            transactions.pluck(:price_per_unit).compact.uniq.size < 2
-                          }
-                          .sort_by { |item_id, transactions|
+                            (transactions.pluck(:price_per_unit).compact.uniq.size < 2) || item_id.blank?
+                          }.group_by { |item_id, transactions|
                             best_ppu = transactions.pluck(:price_per_unit).compact.min
                             worst_ppu = transactions.pluck(:price_per_unit).compact.max
                             most_recent_transaction = transactions.sort_by{|t| t.receipt.date}.reverse.first
 
                             if most_recent_transaction.price_per_unit == best_ppu
-                              1
+                              'best-transactions'
                             elsif most_recent_transaction.price_per_unit == worst_ppu
-                              -1
+                              'worst-transactions'
                             else
-                              0
+                              'unknown-transactions'
                             end
                           }
   end
