e0f1c887469eeeee6279751ce60cf9c73e96434f
Xavier Hocquet <xhocquet@gmail.com>
2017-09-29 16:41:52 -0400

    Remove config

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ module GroceryGuruServer
     config.load_defaults 5.1
 
     config.to_prepare do
-      Devise::Mailer.send(:include, Roadie::Rails::Automatic)
       Devise::Mailer.layout 'mailer' #specify the layout
       # Devise::Mailer.helper 'email' #only if needed!
     end
