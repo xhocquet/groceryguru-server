05c53a412afa379be5749f2ab8d08173e32467b0
Xavier Hocquet <xhocquet@gmail.com>
2017-09-30 11:25:45 -0400

    Change PDF storage folder for prod

--- app/uploaders/receipt_pdf_uploader.rb
+++ app/uploaders/receipt_pdf_uploader.rb
@@ -2,7 +2,7 @@ class ReceiptPDFUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_whitelist
