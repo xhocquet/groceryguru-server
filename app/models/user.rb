d954601d3ecb7cb8609db15c101e983481235ca1
Xavier Hocquet <xhocquet@gmail.com>
2017-10-23 10:25:46 -0400

    Fix google flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,17 @@ class User < ApplicationRecord
   has_many :submissions, dependent: :destroy, inverse_of: :user
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.skip_confirmation!
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
     end
+    user.skip_confirmation!
+    user
   end
 end
