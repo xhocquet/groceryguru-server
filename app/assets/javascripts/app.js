1d0c0eb98045796871e1279a6a81d083db9225e4
Xavier Hocquet <xhocquet@gmail.com>
2017-12-16 22:52:19 -0700

    Raven JS

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  Raven.config('https://550e360a2a7c4820affa2ea8b61ecbdd@sentry.io/260445').install();
+
   $('.notification .delete').click(function(e) {
     $(e.target).parents('.notification').removeClass('show');
   });
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,6 @@
-Raven.configure do |config|
-  config.dsn = 'https://3b87f42ab5d0475ab876dd4e72823e13:9f367fea6fdc46d7a96319dae51e5e75@sentry.io/260444'
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = 'https://3b87f42ab5d0475ab876dd4e72823e13:9f367fea6fdc46d7a96319dae51e5e75@sentry.io/260444'
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
